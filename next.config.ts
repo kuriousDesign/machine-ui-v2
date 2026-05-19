685a4541bf09bd50697991c20359079e08f3d7e1
kuriousdesign <gardner.761@gmail.com>
2026-05-19 16:24:08 +0000

    good commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["10.70.70.50"],
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
